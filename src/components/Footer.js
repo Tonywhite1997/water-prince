ad2edbc54702560cebdcf5e7fdd9f7dfe500fe84
Tonywhite1997 <tonywhite1997814.tw@gmail.com>
2023-05-16 12:45:00 +0100

    added hero section to the homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Footer() {
-  return <div className="footer">footer</div>;
+  return (
+    <footer className="footer">
+      <div className="social">
+        <div className="social-links">
+          <i className="fa-brands fa-facebook"></i>
+          <i className="fa-brands fa-twitter"></i>
+          <i className="fa-brands fa-instagram"></i>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import LandingVideo from "../assets/heroVideo/landing-video.mp4";
 
 function LandingPage() {
+  function scrollToTop() {
+    return window.scroll(0, 0);
+  }
   return (
-    <div className="landing-page">
-      <p>landing page</p>
-    </div>
+    <main className="landing-page">
+      <section className="landing-page__video">
+        <video autoPlay loop muted playsInline>
+          <source src={LandingVideo} type="video/mp4" />
+        </video>
+        <div className="overlay"></div>
+        <div className="text-container">
+          <h1 className="headline">
+            Unlocking Access to Clean Water: Borehole Drilling Experts
+          </h1>
+          <button>
+            <Link to="/contact-us" className="cta-button" onClick={scrollToTop}>
+              Get In Touch
+            </Link>
+          </button>
+        </div>
+      </section>
+    </main>
   );
 }
 
